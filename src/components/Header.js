b258fad419e92a4f682bda664e03803af6a08c3f
Karthik Shetty <86142251+KarthikShetty11@users.noreply.github.com>
2022-06-14 12:10:17 +0530

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ function Header() {
   return (
     <div className="App-header">
       <h1>Contacts</h1>
-      <h2>find all your conact deatials here !</h2>
+      <h2>find all your superhero contact deatials here !</h2>
     </div>
   );
 }
