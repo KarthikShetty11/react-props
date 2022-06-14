8b3887246e7a10483010c2b3206119088e76ada5
Karthik Shetty <86142251+KarthikShetty11@users.noreply.github.com>
2022-06-14 12:08:49 +0530

    contacts component updated

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -2,28 +2,26 @@ const contacts = [
   {
     name: "iron man",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_9Xcq66vg8TUarjVH51yU-i3KC9mWXZjuU7xjmANCcbesUnhi3gGgus2UiC5O9Xp3GAY&usqp=CAU",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThSbrNr49IRS-0OHTIlbnVytbzO20dKwwLgI59LZT2Z9J-MOp5L_mz0aJM0S93symoOO8&usqp=CAU",
     address: "Newyork",
     phone: "1651515"
   },
   {
     name: "batman",
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_9Xcq66vg8TUarjVH51yU-i3KC9mWXZjuU7xjmANCcbesUnhi3gGgus2UiC5O9Xp3GAY&usqp=CAU",
+      "https://5.imimg.com/data5/SELLER/Default/2021/12/RW/TJ/WJ/124914915/51htuha3uds-sl1500--1000x1000.jpg",
     address: "dc",
     phone: "1651515"
   },
   {
     name: "superman",
-    img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_9Xcq66vg8TUarjVH51yU-i3KC9mWXZjuU7xjmANCcbesUnhi3gGgus2UiC5O9Xp3GAY&usqp=CAU",
+    img: "https://images.artwanted.com/large/02/43544_1089002.jpg",
     address: "california",
     phone: "1651515"
   },
   {
     name: "antman",
-    img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_9Xcq66vg8TUarjVH51yU-i3KC9mWXZjuU7xjmANCcbesUnhi3gGgus2UiC5O9Xp3GAY&usqp=CAU",
+    img: "https://thumbs.dreamstime.com/b/print-178010363.jpg",
     address: "florida",
     phone: "1651515"
   }
